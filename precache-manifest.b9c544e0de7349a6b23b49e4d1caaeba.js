3bc55723d3ad90e4267be19dbdaf620be11dfc52
Alex Brajkovic <alex.b@pannovate.com>
2021-06-15 00:28:22 +0200

    Updates

---
+++ precache-manifest.b9c544e0de7349a6b23b49e4d1caaeba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59f13494a3614dc097c48f0ff671c71",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9edb8b1ff942e370247a",
+    "url": "./static/css/main.b16fefee.chunk.css"
+  },
+  {
+    "revision": "394342a30d0107e91678",
+    "url": "./static/js/2.ed7e2907.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ed7e2907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9edb8b1ff942e370247a",
+    "url": "./static/js/main.18286b5b.chunk.js"
+  },
+  {
+    "revision": "6b38cd5eed20a59b29bf",
+    "url": "./static/js/runtime-main.ed253372.js"
+  }
+]);
